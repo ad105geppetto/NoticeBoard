4cc216ad0a42d4cfb911205cbc262e45328893b0
ad105geppetto <ad105geppetto@gmail.com>
2022-09-07 18:56:51 +0900

    feat: 게시판 글 PATCH 요청 라우터 추가

--- src/noticeBoards/router.js
+++ src/noticeBoards/router.js
@@ -5,4 +5,5 @@ const router = express.Router();
 router.get("/boards", controller.getAll);
 router.get("/boards/:id", controller.getOne);
 router.post("/boards", controller.post);
+router.patch("/boards/:id", controller.patch);
 module.exports = router;
