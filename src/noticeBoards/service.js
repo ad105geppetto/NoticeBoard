64285db123f47a08955ed190bedecd92dabcd67e
ad105geppetto <ad105geppetto@gmail.com>
2022-09-07 16:17:26 +0900

    feat: 게시판 서비스 수정

--- src/noticeBoards/service.js
+++ src/noticeBoards/service.js
@@ -3,9 +3,10 @@ const repository = require("./repository");
 module.exports = {
   /**
    * 함수 설명
+   * @param {{order:string, page:number, limit: number}} object 무한 스크롤 변수 모음
    * @returns 전체 게시글 레포지토리 반환
    */
-  getAll: async () => {
-    return await repository.getAll();
+  getAll: async (infiniteScrollingData) => {
+    return await repository.getAll(infiniteScrollingData);
   },
 };
