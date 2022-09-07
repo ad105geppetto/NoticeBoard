dbf03dcb4ce746511cd136a48e77b00058e35adf
ad105geppetto <ad105geppetto@gmail.com>
2022-09-07 17:11:20 +0900

    feat: 게시판 글 비밀번호 암호화 모듈 추가

---
+++ src/noticeBoards/modules/hashPassword.js
@@ -0,0 +1,14 @@
+const crypto = require("crypto");
+
+const hashPassword = (password) => {
+  const salt = crypto.randomBytes(64).toString("base64");
+  const iterations = 10000;
+  const hash = crypto.pbkdf2Sync(password, salt, iterations, 64, "sha256").toString("base64");
+
+  return {
+    salt: salt,
+    hash: hash,
+  };
+};
+
+export default hashPassword;
