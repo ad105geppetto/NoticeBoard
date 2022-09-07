f1b6302cf4f02b9f3bf6445e7c12bae260263585
ad105geppetto <ad105geppetto@gmail.com>
2022-09-07 16:19:05 +0900

    chore: 주석 수정

--- src/noticeBoards/repository.js
+++ src/noticeBoards/repository.js
@@ -5,7 +5,7 @@ module.exports = {
   /**
    * 함수 설명
    * @param {{order:string, page:number, limit: number}} object 무한 스크롤 변수 모음
-   * @returns 전체 공지사항 게시글 레포지토리 반환
+   * @returns 전체 게시글 레포지토리 반환
    */
   getAll: async (infiniteScrollingData) => {
     const { order, page, limit } = infiniteScrollingData;
