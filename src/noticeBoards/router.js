29f761b9615ae6cf26491d45e461d84c1726fd59
ad105geppetto <ad105geppetto@gmail.com>
2022-09-07 16:21:49 +0900

    feat: 특정 게시판 글 GET 요청 라우터 추가

--- src/noticeBoards/router.js
+++ src/noticeBoards/router.js
@@ -3,4 +3,5 @@ const controller = require("./controller");
 const router = express.Router();
 
 router.get("/boards", controller.getAll);
+router.get("/boards/:id", controller.getOne);
 module.exports = router;
