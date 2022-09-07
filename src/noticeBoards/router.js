69d66b73f6c0f0835e7701be7a9e96e9efd464c2
ad105geppetto <ad105geppetto@gmail.com>
2022-09-07 19:43:14 +0900

    feat: 게시판 글 DELETE 요청 라우터 추가

--- src/noticeBoards/router.js
+++ src/noticeBoards/router.js
@@ -6,4 +6,5 @@ router.get("/boards", controller.getAll);
 router.get("/boards/:id", controller.getOne);
 router.post("/boards", controller.post);
 router.patch("/boards/:id", controller.patch);
+router.delete("/boards/:id", controller.delete);
 module.exports = router;
